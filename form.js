47e9179375d267472e5cce8fea9aebc50168f950
genki utsumi <zabanya0770@gmail.com>
2023-06-13 22:37:31 +0900

    実装完了

--- form.js
+++ form.js
@@ -16,6 +16,9 @@ new Vue({
     // methodsに、Vue.jsのインスタンスに使用させたいメソッドを記載する
     methods: {
       addStudent: function() {
+        this.defaultLastId = this.defaultLastId + 1
+
+        this.students.push({id: this.defaultLastId, name: this.name, course: this.course, acceptancePeriod: this.acceptancePeriod })
           // この function() { } の中にaddStudentメソッド
           // （入力欄に入力された値と、ID値（最後に登録されたID値+1の値）が新たに出現する機能）の処理を書く
       }
